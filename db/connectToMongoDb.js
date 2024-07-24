2288759bf5a171d8315ac251cb0a14a15d66ed25
Mradul <mradul9936@gmail.com>
2024-07-25 01:52:28 +0530

    commit

--- db/connectToMongoDb.js
+++ db/connectToMongoDb.js
@@ -2,6 +2,7 @@ import {connect} from 'mongoose';
 
 export default async function conectToMongoDb(res){
     try{
+        console.log(process.env.MONGODB_URI)
         await connect(process.env.MONGODB_URI)
         console.log("MongoDb Connected")
 
